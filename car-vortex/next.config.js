6ca99ffa7b66170dec3fa4429dd75223dd07a0fa
adam <adam.guerchi@tek-up.de>
2024-09-08 11:57:17 +0100

    Refactor next.config.js to add environment variables for Clerk authentication and webhooks

--- car-vortex/next.config.js
+++ car-vortex/next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
