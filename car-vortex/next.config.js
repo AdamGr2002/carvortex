ad3da0b439c4e1ca0d984724a4184abd982decd2
adam <adam.guerchi@tek-up.de>
2024-09-08 11:07:32 +0100

    Refactor next.config.js to add DIRECT_URL environment variable

--- car-vortex/next.config.js
+++ car-vortex/next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
